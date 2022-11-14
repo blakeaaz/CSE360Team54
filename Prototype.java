8a2607e8fa73ad6459bab54af377d3b8096c11a8
AnthonyBibbo <118140090+AnthonyBibbo@users.noreply.github.com>
2022-11-13 21:01:20 -0700

    Update Prototype.java

--- Prototype.java
+++ Prototype.java
@@ -1,4 +1,4 @@
-import javafx.application.Application;
+import javafx.application.Application;			//import necessary javafx utilities
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
